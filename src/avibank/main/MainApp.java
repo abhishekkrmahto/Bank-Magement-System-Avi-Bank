7a80bf2d93036e02fe88e557f6d7643a1d015ef4
avishekkrmahto <abhishek.mahto2025@gmail.com>
2025-12-28 18:51:48 +0530

    added welcome messeage

---
+++ src/avibank/main/MainApp.java
@@ -0,0 +1,16 @@
+package avibank.main;
+
+import avibank.welcomeScreen.Welcome;
+
+public class MainApp {
+
+    public static void main(String[] args) throws InterruptedException {
+        // IMPORTS AND OBJECTS
+        Welcome welcome = new Welcome();
+
+        // WELCOME SCREEN
+        welcome.welcomeScreen();
+
+    }
+
+}
---
+++ src/avibank/welcomeScreen/Welcome.java
@@ -0,0 +1,22 @@
+package avibank.welcomeScreen;
+
+public class Welcome {
+    public void welcomeScreen() throws InterruptedException {
+        // WELCOME TO AVI BANK SERVICE
+        System.out.println("-------------------------------------------------------------------------\n");
+        System.out.print("                     ");
+        Thread.sleep(300);
+        System.out.print("WELCOME ");
+        Thread.sleep(300);
+        System.out.print("TO ");
+        Thread.sleep(300);
+        System.out.print("AVI ");
+        Thread.sleep(300);
+        System.out.print("BANK ");
+        Thread.sleep(300);
+        System.out.print("SERVICE ");
+        Thread.sleep(300);
+        System.out.println("                     \n");
+        System.out.println("-------------------------------------------------------------------------");
+    }
+}
